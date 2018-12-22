ff6e432c4e8800bcb969ec3dc431f425160fb2fe
Charlie McClung <charlie@cmr1.com>
2018-12-21 18:28:52 -0700

    Tweak build script

--- build.sh
+++ build.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+echo "Building: Test React App"
+
+echo "Installing NPM dependencies..."
+
 npm install
+
+echo "Finished installing node_modules"
+
+echo "Running test suite..."
 
 npm test
 
+echo "Finished test suite"
+
+echo "Run production build..."
+
 GENERATE_SOURCEMAP=false npm run build
+
+echo "Finished production build"
+
+echo "Sleep a bit ... for good measure"
+
+sleep 60
+
+echo "DONE"
